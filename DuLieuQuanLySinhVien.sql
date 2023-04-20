daff165165b7cda20d5f56683d387a9c250777bb
LucDuong1720 <lucduong1720@gmail.com>
2023-04-20 15:11:46 +0700

    module3

--- DuLieuQuanLySinhVien.sql
+++ DuLieuQuanLySinhVien.sql
@@ -1,1 +1,30 @@
-SELECT * FROM quanlysinhvien.mark;
+insert into Class
+value (1, 'A1', '2008-12-20', 1);
+insert into CLass
+value (2, 'A2', '2008-12-22', 1);
+insert into Class
+value (3, 'B3', Current_Date, 0); 
+
+insert into student
+value (1, 'Hung', 'Ha noi', '0912113113', 1, 1);
+insert into student
+value (2, 'Hoa', 'Hai phong', null, 1, 1);
+insert into student
+value (3, 'Manh', 'HCM', '0123123123', 0, 2);
+
+insert into `subject`
+value (1, 'CF', 5, 1);
+insert into `subject`
+value (2, 'C', 6, 1);
+insert into `subject`
+value (3, 'HDJ', 5, 1);
+insert into `subject`
+value (4, 'RDBMS', 10, 1);
+
+insert into Mark
+value (1, 1, 1, 8, 1);
+insert into Mark
+value (2, 1, 2, 10, 2);
+insert into Mark
+value (3, 2, 1, 12, 1);
+
